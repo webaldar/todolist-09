0b26c418473a93f3866daf3a501833dc0f7791ee
webaldar <webaldar@gmail.com>
2025-06-04 16:15:42 +0300

    refactor todolists-reducer.ts part2

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -11,28 +11,22 @@ export const todolistsReducer = createReducer(initialState, builder =>{
           state.splice(index, 1)
         }
       })
+      .addCase(createTodolistAC, (state, action) => {
+          state.push({...action.payload, filter: "all"})
+      })
+      .addCase(changeTodolistTitleAC, (state, action) => {
+          const index = state.findIndex(todolist => todolist.id === action.payload.id)
+          if(index !== -1){
+              state[index].title = action.payload.title
+          }
+      })
+      .addCase(changeTodolistFilterAC, (state, action) => {
+          const todolist = state.find(todo => todo.id === action.payload.id)
+          if(todolist){
+              todolist.filter = action.payload.filter
+          }
+      })
 })
-
-// export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
-//   switch (action.type) {
-//     case 'delete_todolist': {
-//       return state.filter(todolist => todolist.id !== action.payload.id)
-//     }
-//     case 'create_todolist': {
-//       const newTodolist: Todolist = {id: action.payload.id, title: action.payload.title, filter: 'all'}
-//       return [...state, newTodolist]
-//     }
-//     case 'change_todolist_title': {
-//       return state.map(todolist => todolist.id === action.payload.id ? {...todolist, title: action.payload.title} : todolist)
-//     }
-//     case 'change_todolist_filter': {
-//       return state.map(todolist => todolist.id === action.payload.id ? {...todolist, filter: action.payload.filter} : todolist)
-//     }
-//     default:
-//       return state
-//   }
-// }
-
 export const deleteTodolistAC = createAction<{id: string}>('todolists/deleteTodolist')
 
 export const createTodolistAC = createAction('todolists/createTodolist', (title: string) => {
@@ -45,11 +39,9 @@ export const changeTodolistFilterAC = createAction<{id: string, filter: FilterVa
 
 export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
 export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
-export type ChangeTodolistTitleAction = ReturnType<typeof changeTodolistTitleAC>
-export type ChangeTodolistFilterAction = ReturnType<typeof changeTodolistFilterAC>
+
 
 type Actions =
     | DeleteTodolistAction
     | CreateTodolistAction
-    | ChangeTodolistTitleAction
-    | ChangeTodolistFilterAction
+
